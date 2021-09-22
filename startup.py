a6c12cfe3f949a5adc2b490c2e35b32a7aca58c3
TheSmetz <matteo.guerrini@studenti.unicam.it>
2021-09-22 11:57:31 +0200

    Templates working

--- startup.py
+++ startup.py
@@ -1,13 +1,12 @@
+import re
 from flask import Flask, render_template, send_file, redirect, request
 from flask.helpers import make_response
 from docx import Document
 from datetime import date
 
-# TODO: FIX THE PROBLEM WITH DOC
-
-
-# TODO SECOND ENDPOINT
-
+# TODO: FIX THE PROBLEM WITH DOC+
+# TODO: ASK FOR OTHER DOCUMENTS
+# TODO: BEAUTIFY UI
 
 app = Flask(__name__)
 global doc
@@ -17,7 +16,6 @@ today = date.today()
 @app.route('/')
 def home():
     return render_template('home.html')
-
 
 @app.route('/docmaker', methods=['POST'])
 def pdf_template():
@@ -83,7 +81,6 @@ def proofofparticipationinenglishcourses():
         return redirect('/download')
 
 
-# TODO: CAPS LOCK THE COUNTRY!
 @app.route('/confirmationofunconditionaladmission', methods=['GET', 'POST'])
 def confirmationofunconditionaladmission():
     global doc
@@ -92,9 +89,250 @@ def confirmationofunconditionaladmission():
     else:
         gender = request.form.get('genderattrs')
         name = request.form.get('name') + " " + request.form.get('surname')
+        country = request.form.get('countryattribute').upper()
+        city = request.form.get('cityattribute')
+        zipcode = request.form.get('zipcode')
+        address1 = request.form.get('address1')
+        address2 = request.form.get('address2')
+        number_address = request.form.get('numberaddress')
         date = today.strftime("%d %B %Y")
         years = request.form.get('yearsattribute')
-        studentID = request.form.get('idstudentattribute')
+        semester = request.form.get('semesters')
+        degreetype = request.form.get('degreetypes')
+        degreefield = request.form.get('degreefield')
+        for p in doc.paragraphs:
+            inline = p.runs
+            for i in range(len(inline)):
+                text = inline[i].text
+                if 'nameattr' in text:
+                    text = text.replace('nameattr', name)
+                    inline[i].text = text
+                if 'dateattribute' in text:
+                    text = text.replace('dateattribute', date)
+                    inline[i].text = text
+                if 'countryattribute' in text:
+                    text = text.replace('countryattribute', country)
+                    inline[i].text = text
+                if 'zipcode' in text:
+                    text = text.replace('zipcode', zipcode)
+                    inline[i].text = text
+                if 'cityattribute' in text:
+                    text = text.replace('cityattribute', city)
+                    inline[i].text = text
+                if 'numberaddress' in text:
+                    text = text.replace('numberaddress', number_address)
+                    inline[i].text = text
+                if 'address1attribute' in text:
+                    text = text.replace('address1attribute', address1)
+                    inline[i].text = text
+                if 'address2attribute' in text:
+                    text = text.replace('address2attribute', address2)
+                    inline[i].text = text
+                if 'semester' in text:
+                    text = text.replace('semester', semester.capitalize())
+                    inline[i].text = text
+                if 'yearsattribute' in text:
+                    text = text.replace('yearsattribute', years)
+                    inline[i].text = text
+                if 'genderattr' in text:
+                    text = text.replace('genderattr', gender.capitalize())
+                    inline[i].text = text
+                if 'degreefield' in text:
+                    text = text.replace(
+                        'degreefield', degreefield.capitalize())
+                    inline[i].text = text
+                if 'type' in text:
+                    if degreetype == 'bsc':
+                        degreetype = 'B.Sc.'
+                    else:
+                        degreetype = 'M.Sc.'
+                    print(degreetype)
+                    text = text.replace('type', degreetype)
+                    inline[i].text = text
+        doc.save('output.docx')
+        return redirect('/download')
+
+
+@app.route('/extensionletter', methods=['GET', 'POST'])
+def extensionletter():
+    global doc
+    if request.method == 'GET':
+        return render_template('extensionletter.html')
+    else:
+        name = request.form.get('name') + " " + request.form.get('surname')
+        date = today.strftime("%d %B %Y")
+        years = request.form.get('yearsattr')
+        semester = request.form.get('semesters')
+        applicationnum = request.form.get('appnum')
+        for p in doc.paragraphs:
+            inline = p.runs
+            for i in range(len(inline)):
+                text = inline[i].text
+                if 'nameattr' in text:
+                    text = text.replace('nameattr', name)
+                    inline[i].text = text
+                if 'dateattribute' in text:
+                    text = text.replace('dateattribute', date)
+                    inline[i].text = text
+                if 'semestera' in text:
+                    text = text.replace('semestera', semester.capitalize())
+                    inline[i].text = text
+                if 'appnum' in text:
+                    text = text.replace('appnum', applicationnum)
+                    inline[i].text = text
+                if 'yearsattr' in text:
+                    text = text.replace('yearsattr', years)
+                    inline[i].text = text
+        doc.save('output.docx')
+        return redirect('/download')
+
+
+@app.route('/latearrival', methods=['GET', 'POST'])
+def latearrival():
+    global doc
+    if request.method == 'GET':
+        return render_template('latearrival.html')
+    else:
+        gender = request.form.get('genderattrs')
+        name = request.form.get('name') + " " + request.form.get('surname')
+        country = request.form.get('countryattribute').upper()
+        city = request.form.get('cityattribute')
+        zipcode = request.form.get('zipcode')
+        address1 = request.form.get('address1')
+        address2 = request.form.get('address2')
+        number_address = request.form.get('numberaddress')
+        date = today.strftime("%d %B %Y")
+        studentid = request.form.get('studentid')
+        degreefield = request.form.get('degreefield')
+        for p in doc.paragraphs:
+            inline = p.runs
+            for i in range(len(inline)):
+                text = inline[i].text
+                if 'nameattr' in text:
+                    text = text.replace('nameattr', name)
+                    inline[i].text = text
+                if 'dateattribute' in text:
+                    text = text.replace('dateattribute', date)
+                    inline[i].text = text
+                if 'countryattribute' in text:
+                    text = text.replace('countryattribute', country)
+                    inline[i].text = text
+                if 'zipcode' in text:
+                    text = text.replace('zipcode', zipcode)
+                    inline[i].text = text
+                if 'cityattribute' in text:
+                    text = text.replace('cityattribute', city)
+                    inline[i].text = text
+                if 'numberaddress' in text:
+                    text = text.replace('numberaddress', number_address)
+                    inline[i].text = text
+                if 'address1attribute' in text:
+                    text = text.replace('address1attribute', address1)
+                    inline[i].text = text
+                if 'address2attribute' in text:
+                    text = text.replace('address2attribute', address2)
+                    inline[i].text = text
+                if 'genderattr' in text:
+                    text = text.replace('genderattr', gender.capitalize())
+                    inline[i].text = text
+                if 'studentid' in text:
+                    text = text.replace(
+                        'studentid', studentid)
+                    inline[i].text = text
+                if 'degreefield' in text:
+                    text = text.replace(
+                        'degreefield', degreefield.capitalize())
+                    inline[i].text = text
+        doc.save('output.docx')
+        return redirect('/download')
+
+
+@app.route('/blockedaccount', methods=['GET', 'POST'])
+def blockedaccount():
+    global doc
+    if request.method == 'GET':
+        return render_template('blockedaccount.html')
+    else:
+        date = today.strftime("%d %B %Y")
+        years = request.form.get('yearsattr')
+        semester = request.form.get('semesters')
+        for p in doc.paragraphs:
+            inline = p.runs
+            for i in range(len(inline)):
+                text = inline[i].text
+                if 'dateattribute' in text:
+                    text = text.replace('dateattribute', date)
+                    inline[i].text = text
+                if 'semestera' in text:
+                    if semester == 'winter':
+                        startingdate = '1st October'
+                    else:
+                        startingdate = '1st April'
+                    text = text.replace('semestera', semester.capitalize())
+                    inline[i].text = text
+                if 'startingdate' in text:
+                    text = text.replace('startingdate', startingdate)
+                    inline[i].text = text
+                if 'yearsattr' in text:
+                    text = text.replace('yearsattr', years)
+                    inline[i].text = text
+        doc.save('output.docx')
+        return redirect('/download')
+
+
+@app.route('/onlinesemesterparticipation', methods=['GET', 'POST'])
+def onlinesemesterparticipation():
+    global doc
+    if request.method == 'GET':
+        return render_template('onlinesemesterparticipation.html')
+    else:
+        gender = request.form.get('genderattrs')
+        name = request.form.get('name') + " " + request.form.get('surname')
+        matricnumb = request.form.get('matricnumb')
+        numonlinecourse = request.form.get('numonlinecourse')
+        date = today.strftime("%d %B %Y")
+        semester = request.form.get('semesters')
+        year = request.form.get('yearsattribute')
+        for p in doc.paragraphs:
+            inline = p.runs
+            for i in range(len(inline)):
+                text = inline[i].text
+                if 'nameattr' in text:
+                    text = text.replace('nameattr', name)
+                    inline[i].text = text
+                if 'dateattribute' in text:
+                    text = text.replace('dateattribute', date)
+                    inline[i].text = text
+                if 'matricnum' in text:
+                    text = text.replace('matricnum', matricnumb)
+                    inline[i].text = text
+                if 'semesterattr' in text:
+                    text = text.replace('semesterattr', semester.capitalize())
+                    inline[i].text = text
+                if 'genderattr' in text:
+                    text = text.replace('genderattr', gender.capitalize())
+                    inline[i].text = text
+                if 'numonlinecourse' in text:
+                    text = text.replace(
+                        'numonlinecourse', numonlinecourse)
+                    inline[i].text = text
+                if 'yearatt' in text:
+                    text = text.replace(
+                        'yearatt', year)
+                    inline[i].text = text
+        doc.save('output.docx')
+        return redirect('/download')
+
+
+@app.route('/presenceletter', methods=['GET', 'POST'])
+def presenceletter():
+    global doc
+    if request.method == 'GET':
+        return render_template('presenceletter.html')
+    else:
+        name = request.form.get('name') + " " + request.form.get('surname')
+        date = today.strftime("%d %B %Y")
+        years = request.form.get('yearsattr')
         semester = request.form.get('semesters')
         for p in doc.paragraphs:
             inline = p.runs
@@ -106,52 +344,57 @@ def confirmationofunconditionaladmission():
                 if 'dateattribute' in text:
                     text = text.replace('dateattribute', date)
                     inline[i].text = text
-                if 'semesterattribute' in text:
-                    text = text.replace('semesterattribute', semester)
+                if 'semesterattr' in text:
+                    text = text.replace('semesterattr', semester.capitalize())
                     inline[i].text = text
-                if 'yearsattribute' in text:
-                    text = text.replace('yearsattribute', years)
-                    inline[i].text = text
-                if 'genderattribute' in text:
-                    print(gender.capitalize())
-                    text = text.replace('genderattribute', gender.capitalize())
-                    inline[i].text = text
-                if 'idstudentattribute' in text:
-                    text = text.replace('idstudentattribute', studentID)
+                if 'yearsattr' in text:
+                    text = text.replace('yearsattr', years)
                     inline[i].text = text
         doc.save('output.docx')
         return redirect('/download')
 
 
-@app.route('/extensionletter')
-def extensionletter():
-    return render_template('extensionletter.html')
-
-
-@app.route('/latearrival')
-def latearrival():
-    return render_template('latearrival.html')
-
-
-# TODO: CHECK the starting date for winter and summer semester to auto-generate it.
-@app.route('/blockedaccount')
-def blockedaccount():
-    return render_template('blockedaccount.html')
-
-
-@app.route('/onlinesemesterparticipation')
-def onlinesemesterparticipation():
-    return render_template('onlinesemesterparticipation.html')
-
-
-@app.route('/presenceletter')
-def presenceletter():
-    return render_template('presenceletter.html')
-
-
-@app.route('/proofoflanguagerequirements')
+@app.route('/proofoflanguagerequirements', methods=['GET', 'POST'])
 def proofoflanguagerequirements():
-    return render_template('proofoflanguagerequirements.html')
+    global doc
+    if request.method == 'GET':
+        return render_template('proofoflanguagerequirements.html')
+    else:
+        name = request.form.get('name') + " " + request.form.get('surname')
+        date = today.strftime("%d %B %Y")
+        years = request.form.get('yearsattr')
+        matnum = request.form.get('matnum')
+        degreetype = request.form.get('degreetypes')
+        degreefield = request.form.get('degreefield')
+        for p in doc.paragraphs:
+            inline = p.runs
+            for i in range(len(inline)):
+                text = inline[i].text
+                if 'nameattr' in text:
+                    text = text.replace('nameattr', name)
+                    inline[i].text = text
+                if 'dateattribute' in text:
+                    text = text.replace('dateattribute', date)
+                    inline[i].text = text
+                if 'yearsattr' in text:
+                    text = text.replace('yearsattr', years)
+                    inline[i].text = text
+                if 'matnum' in text:
+                    text = text.replace('matnum', matnum)
+                    inline[i].text = text
+                if 'degreefield' in text:
+                    text = text.replace(
+                        'degreefield', degreefield.capitalize())
+                    inline[i].text = text
+                if 'tipolaurea' in text:
+                    if degreetype == 'bsc':
+                        degreetype = 'B.Sc.'
+                    else:
+                        degreetype = 'M.Sc.'
+                    text = text.replace('tipolaurea', degreetype)
+                    inline[i].text = text
+        doc.save('output.docx')
+        return redirect('/download')
 
 
 @app.route('/download')
